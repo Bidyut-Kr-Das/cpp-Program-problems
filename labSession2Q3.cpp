7839797716803c23bb6e440df26bf6e4dba7e0b5
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2023-02-15 12:56:55 +0530

    on progress

---
+++ labSession2Q3.cpp
@@ -0,0 +1,13 @@
+/*Question: Consider the following structure for Distance and Room
+struct Distance{
+    int feet;
+    int inches;
+};
+struct Room{
+    Distance length;
+    Distance width;
+}
+(a)Create an object of struct Room as dining_room.
+(b)Take the dimension of the room as input from the user.
+(c)Print the area of the room as square feet.
+*/
